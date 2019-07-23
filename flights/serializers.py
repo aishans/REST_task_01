f54578cb7dd5b93f6b44735ec25f55b5374ca286
Alia <Alia@Ys-MacBook-Pro.local>
2019-07-23 18:55:40 +0300

    task1

---
+++ flights/serializers.py
@@ -0,0 +1,12 @@
+from rest_framework import serializers
+from .models import Flight, Booking
+
+class FlightSerializer(serializers.ModelSerializer):
+	class Meta:
+		model = Flight
+		exclude = ['miles']
+
+class BookingSerializer(serializers.ModelSerializer):
+	class Meta:
+		model = Booking
+		exclude = ['user', 'passengers']
--- flights/views.py
+++ flights/views.py
@@ -1,1 +1,12 @@
+from django.shortcuts import render
+from rest_framework.generics import ListAPIView
 from .models import Flight, Booking
+from .serializers import *
+
+class FlightsList(ListAPIView):
+	queryset = Flight.objects.all()
+	serializer_class = FlightSerializer
+
+class BookingList(ListAPIView):
+	queryset = Booking.objects.all()
+	serializer_class = BookingSerializer
